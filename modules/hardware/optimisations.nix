2901e95c10b1295a1bc12c77ed5ab41e83ad5ad4
Lazres31 <lazres31@yandex.ru>
2025-05-23 23:16:25 +0300

    Setup Intel

--- modules/hardware/optimisations.nix
+++ modules/hardware/optimisations.nix
@@ -3,6 +3,7 @@
 {
 	boot.kernelParams = [
 		"pcie_aspm=force"
+		"i915.enable_guc=3"
 	];
 	boot.initrd = {
 		availableKernelModules = ["i915"];
@@ -10,5 +11,29 @@
 	};
 	hardware.cpu.intel.updateMicrocode = true;
 
+	# for games
 	boot.kernel.sysctl."vm.max_map_count" = 2147483642;
+	boot.kernel.sysctl."dev.i915.perf_stream_paranoid" = 0;	
+
+	# INTEL
+	hardware.graphics = {
+		enable = true;
+		extraPackages = with pkgs; [
+			vpl-gpu-rt
+			intel-vaapi-driver
+			intel-media-driver
+			libvdpau-va-gl
+		];
+	};
+	
+	hardware.intel-gpu-tools.enable = true;
+	environment.systemPackages = with pkgs; [
+		intel-gpu-tools
+		vdpauinfo
+		libva-utils
+		vulkan-tools
+	];
+
+	# NVIDIA
+	
 }
