810bd7ba769a00e222f36581c6503227e12359bd
Lazres31 <lazres31@yandex.ru>
2025-06-03 16:23:52 +0300

    Add autologin into sddm

--- modules/programs/plasma.nix
+++ modules/programs/plasma.nix
@@ -3,7 +3,13 @@
 {
 	services = {
 		desktopManager.plasma6.enable = true;
-		displayManager.sddm.enable = true;
-		displayManager.sddm.wayland.enable = true;
+		displayManager.sddm = {
+			enable = true;
+			wayland.enable = true;
+		};
+		displayManager.autoLogin = {
+			enable = true;
+			user = "userok";
+		};
 	};
 }
