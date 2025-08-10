6b5534fa6adc7443e3c891fbd73b4d853fb7a31c
Lazres31 <lazres31@yandex.ru>
2025-08-10 23:40:03 +0300

    Add aliases for flatpak apps

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -15,4 +15,9 @@
 		"md.obsidian.Obsidian"
 		"ru.linux_gaming.PortProton"
 	];
+
+	environment.shellAliases = {
+		portproton = "flatpak run ru.linux_gaming.PortProton";
+		obsidian = "flatpak run md.obsidian.Obsidian";
+	};
 }
