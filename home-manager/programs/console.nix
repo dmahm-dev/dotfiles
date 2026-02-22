4a5741f1d249069c70bd920a044636a2342640d0
Lazres31 <lazres31@yandex.ru>
2026-02-22 21:11:29 +0300

    add kitty

--- home-manager/programs/console.nix
+++ home-manager/programs/console.nix
@@ -6,4 +6,22 @@
 		"shopt -s cdspell histappend histreedit checkjobs\nfastfetch";
 	programs.bash.profileExtra = 
 		''export XDG_DATA_DIRS="$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"'';
+	programs.kitty = {
+		enable = true;
+		enableGitIntegration = true;
+		shellIntegration.enableBashIntegration = true;
+		font.name = "FiraCode Nerd Font";
+		font.size = 10;
+		settings = {
+			disable_ligatures = "cursor";
+			cursor_trail = 3;
+			scrollback_lines = 20000;
+			show_hyperlink_targets = "yes";
+			paste_actions = "quote-urls-at-prompt,confirm,confirm-if-large";
+			#focus_follow_mouse = "yes";
+			confirm_os_window_close = "-1 count-background";
+			tab_activity_symbol = " ";
+			notify_on_cmd_finish = "invisible 15.0 notify-bell";
+		};
+	};
 }
