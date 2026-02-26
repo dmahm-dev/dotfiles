a94d8b6d6811d94c47dbff3c52b282a62849d251
Lazres31 <lazres31@yandex.ru>
2026-02-26 17:34:27 +0300

    Remove unstable branch of nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
 	description = "Entry point flake";
 
 	inputs = {
-		nixpkgsUnstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
 		home-manager = {
 			url = "github:nix-community/home-manager/release-25.11";
@@ -12,19 +11,12 @@
 		nvf.url = "github:notashelf/nvf";
 	};
 
-	outputs = { self, nixpkgs, nixpkgsUnstable, nvf,
-				home-manager, nix-flatpak, ... }@inputs:
+	outputs = { self, nixpkgs, nvf, home-manager, nix-flatpak, ... }@inputs:
 	let
-		unstable = import nixpkgsUnstable {
-			config.allowUnfree = true;
-			config.cudaSupport = true;
-			system = "x86_64-linux";
-		};
 		system = "x86_64-linux";
 	in {
 		nixosConfigurations."SomePC" = inputs.nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
-			specialArgs = { inherit inputs unstable;};
 			modules = [
 				./hosts/main.nix
 				./modules/default.nix
