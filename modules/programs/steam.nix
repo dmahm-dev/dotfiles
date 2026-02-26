7b6ae9ba1bd6c863e1622c6e12a3957d8ebebfed
Lazres31 <lazres31@yandex.ru>
2026-02-26 18:40:03 +0300

    Set steam variables for all games

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -7,12 +7,14 @@
 		dedicatedServer.openFirewall = true;
 		extest.enable = true; # gamepad thing
 		protontricks.enable = true;
-# 		gamescopeSession.enable = true;
-# 		package = pkgs.steam.override {
-# 			extraEnv = {
-# 				MANGOHUD = true;
-# 				OBS_VKCAPTURE = true;
-# 			};
-# 		};
+ 		package = pkgs.steam.override {
+ 			extraEnv = {
+				PROTON_USE_NTSYNC = "1";
+				#PROTON_ENABLE_WAYLAND = "1"; #bad behaviour in outer wilds
+				PROTON_ENABLE_NVAPI = "1"; #fixes video in games
+ 				#MANGOHUD = true;
+ 				#OBS_VKCAPTURE = true;
+ 			};
+ 		};
 	};
 }
