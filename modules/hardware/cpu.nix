7fa28e66b5bde0c7b9ca05e7fede9eb3b099d370
dmahm <dmahm@yandex.ru>
2026-03-03 19:46:56 +0300

    Fixes cpu throttling more correctly

--- modules/hardware/cpu.nix
+++ modules/hardware/cpu.nix
@@ -2,5 +2,8 @@
 
 {
     services.cpupower-gui.enable = true;
-    powerManagement.cpufreq.max = 1600000;
+    systemd.tmpfiles.rules = [ # My CPU is getting hot too quickly
+        "w /sys/devices/system/cpu/intel_pstate/max_perf_pct - - - - 59"
+        "w /sys/devices/system/cpu/intel_pstate/no_turbo - - - - 1"
+    ];
 }
