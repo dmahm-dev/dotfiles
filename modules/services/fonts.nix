8765600f6182cdb58f7875331c74fc13de3a58ce
dmahm <dmahm@yandex.ru>
2026-03-06 12:17:21 +0300

    Add different Chinese font and set default fonts

--- modules/services/fonts.nix
+++ modules/services/fonts.nix
@@ -7,15 +7,15 @@
 		vista-fonts
 		corefonts
 		noto-fonts
-		noto-fonts-cjk-sans
 		noto-fonts-color-emoji
+		lxgw-wenkai
 	];
 
 	fonts.fontDir.enable = true;
 	fonts.fontconfig.defaultFonts = {
-		monospace = [ "FiraCode Nerd Font Mono" ];
-		#sansSerif = [];
-		#serif = [];
-		#emoji = [];
+		monospace = [ "FiraCode Nerd Font Mono" "LXGW WenKai"];
+		sansSerif = [ "Roboto" "LXGW WenKai" ];
+		serif = ["Roboto Slab" "LXGW WenKai"];
+		emoji = ["Noto Color Emoji"];
 	};
 }
