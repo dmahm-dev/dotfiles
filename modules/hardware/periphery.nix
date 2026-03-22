78e3e3c4d51f1c79322db9bd249a97357ebad1cd
dmahm <dmahm@yandex.ru>
2026-03-22 16:54:03 +0300

    Try to setup mice configuration with piper

--- modules/hardware/periphery.nix
+++ modules/hardware/periphery.nix
@@ -1,7 +1,14 @@
-{...}:
+{pkgs, ...}:
 
 {
 	# disable mouse acceleration
 	services.libinput.mouse.accelProfile = "flat";
+	services.ratbagd.enable = true;
 	hardware.xpadneo.enable = true;
+
+	environment.systemPackages = with pkgs; [
+		piper
+		libinput
+		evtest
+	];
 }
